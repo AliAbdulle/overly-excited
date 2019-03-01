c0538b4c10213ebf3b3d9ba70e99ff44504cf4e1
AliAbdulle <alidhobey@gmail.com>
2019-03-01 14:17:49 -0600

    Added text to the new one

---
+++ src/scripts/overly-excited.js
@@ -0,0 +1,21 @@
+let sentence = ["The","walrus","danced","through","the","trees","in","the","light","of","the","moon"];
+const  addExcitement = (theWordArray, makeThesing,numberOfTime) =>{
+    let buildMeUp = ""
+    let wordCounter = 1
+
+    for (let i = 0; i < theWordArray.length; i++) {
+        /*
+            If the current value of `i` divided by 3 has no
+            remainder, add an exclamation point to the end of
+            the word and then concatenate it to `buildMeUp`.
+
+            Otherwise, just concatenate the word itself.
+         */
+
+
+        // Print buildMeUp to the console
+    }
+
+}
+
+addExcitement(sentence)
