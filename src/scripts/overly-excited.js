626e4b27ec3d7cf059a4fb35a4f4fb6b1651630c
AliAbdulle <alidhobey@gmail.com>
2019-03-01 15:49:02 -0600

    Added new works to js file

--- src/scripts/overly-excited.js
+++ src/scripts/overly-excited.js
@@ -1,21 +1,29 @@
 let sentence = ["The","walrus","danced","through","the","trees","in","the","light","of","the","moon"];
-const  addExcitement = (theWordArray, makeThesing,numberOfTime) =>{
+
+/*
+    The addExcitement function should be an impure function, and accept
+    the array as the sole argument. It should iterate over the array
+    and output the words to the browser console.
+*/
+function addExcitement (theWordArray) {
+
+    // Each time the for loop executes, you're going to add one more word to this string
     let buildMeUp = ""
-    let wordCounter = 1
 
     for (let i = 0; i < theWordArray.length; i++) {
-        /*
-            If the current value of `i` divided by 3 has no
-            remainder, add an exclamation point to the end of
-            the word and then concatenate it to `buildMeUp`.
+        if ((i + 1) % 3 === 0 && i !== 0){
+        buildMeUp += theWordArray[i] + "! ";
+        // Concatenate the new word onto buildMeUp
+    }
 
-            Otherwise, just concatenate the word itself.
-         */
-
-
+    else {
+        buildMeUp += theWordArray[i] + " ";
+    }
         // Print buildMeUp to the console
+        console.log(buildMeUp)
     }
 
 }
 
+// Invoke the function and pass in the array
 addExcitement(sentence)
